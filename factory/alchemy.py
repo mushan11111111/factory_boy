f79c330709e30b2e5deb2cd5c93acba8c0d3b3c8
pp345pp <919902877@qq.com>
2026-06-09 09:03:55 +0000

    1

--- factory/alchemy.py
+++ factory/alchemy.py
@@ -60,6 +60,50 @@ class SQLAlchemyModelFactory(base.Factory):
         return super()._generate(strategy, params)
 
     @classmethod
+    def _get_or_create_for_sqlalchemy(cls, model_class, *args, **kwargs):
+        """Create an instance of the model through objects.get_or_create."""
+        manager = cls._get_manager(model_class)
+
+        assert 'defaults' not in cls._meta.django_get_or_create, (
+            "'defaults' is a reserved keyword for get_or_create "
+            "(in %s._meta.django_get_or_create=%r)"
+            % (cls, cls._meta.django_get_or_create))
+
+        key_fields = {}
+        for field in cls._meta.django_get_or_create:
+            if field not in kwargs:
+                raise errors.FactoryError(
+                    "django_get_or_create - "
+                    "Unable to find initialization value for '%s' in factory %s" %
+                    (field, cls.__name__))
+            key_fields[field] = kwargs.pop(field)
+        key_fields['defaults'] = kwargs
+
+        try:
+            instance, _created = manager.get_or_create(*args, **key_fields)
+        except IntegrityError as e:
+
+            if cls._original_params is None:
+                raise e
+
+            get_or_create_params = {
+                lookup: value
+                for lookup, value in cls._original_params.items()
+                if lookup in cls._meta.django_get_or_create
+            }
+            if get_or_create_params:
+                try:
+                    instance = manager.get(**get_or_create_params)
+                except manager.model.DoesNotExist:
+                    # Original params are not a valid lookup and triggered a create(),
+                    # that resulted in an IntegrityError. Follow Django’s behavior.
+                    raise e
+            else:
+                raise e
+
+        return instance
+
+    @classmethod
     def _get_or_create(cls, model_class, session, args, kwargs):
         key_fields = {}
         for field in cls._meta.sqlalchemy_get_or_create:
